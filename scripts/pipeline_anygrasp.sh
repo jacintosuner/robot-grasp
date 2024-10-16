50d884b271699d21a3d8633f41ecf2d3eabf68e2
Jacinto Suner <jacinto@ham.pc.cs.cmu.edu>
2024-10-16 17:23:28 -0400

    making sure the anygrasp pipeline works properly and removing extra data not needed

--- scripts/pipeline_anygrasp.sh
+++ scripts/pipeline_anygrasp.sh
@@ -1,9 +1,16 @@
+#!/bin/bash
+
+# example: ./pipeline_anygrasp.sh /home/jacinto/robot-grasp/data/rgbdks/any_grasp_example.npy
+
+
 export CUDA_HOME=/usr/local/cuda-11.7
 export PATH=/usr/local/cuda-11.7/bin:$PATH
 export LD_LIBRARY_PATH=/usr/local/cuda-11.7/lib64:$LD_LIBRARY_PATH
 
 rgbdk_file_path=${1:-""}
 
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate anygrasp_env
 cd ~/robot-grasp/third_party/anygrasp_sdk/grasp_detection
 
 if [ -n "$rgbdk_file_path" ]; then
