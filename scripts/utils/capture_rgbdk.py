b4412524fa2132a6ff2ec63597373458e04338b0
Lifan Yu <lifany@andrew.cmu.edu>
2024-10-29 11:42:34 -0400

    small change to capture rgbdk code

--- scripts/utils/capture_rgbdk.py
+++ scripts/utils/capture_rgbdk.py
@@ -6,13 +6,13 @@ from pyk4a.calibration import CalibrationType
 import cv2  # Ensure you have OpenCV installed for resizing
 
 # Use
-# python get_camera_inputs.py --output_path ../../data/rgbdks/ --to_npy
-# rsync -r --progress ./robot-grasp/data/rgbdks/rgbdk_data.npy jacinto@ham1.pc.cs.cmu.edu:/home/jacinto/robot-grasp/data/rgbdks
+# python3 capture_rgbdk.py --output_path ../../data/rgbdks/
+# rsync -r --progress ~/robot-grasp/data/rgbdks/rgbdk.npy jacinto@ham1.pc.cs.cmu.edu:/home/jacinto/robot-grasp/data/rgbdks
 
-def capture_rgbd(dir_path, to_images):
-    # Create a dir_path if it doesn't exist
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
+def capture_rgbd(output_path, to_images):
+    # Create a output_path if it doesn't exist
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
 
     # Initialize the Azure Kinect camera with the desired configuration
     k4a = PyK4A()  # Adjust config as needed
@@ -28,8 +28,8 @@ def capture_rgbd(dir_path, to_images):
             
             if to_images:
                 # Save RGB and Depth images as PNG files
-                rgb_path = os.path.join(dir_path, "bgr_frame.png")
-                depth_path = os.path.join(dir_path, "depth_frame.png")
+                rgb_path = os.path.join(output_path, "bgr_frame.png")
+                depth_path = os.path.join(output_path, "depth_frame.png")
 
                 # Save images
                 imageio.imwrite(rgb_path, capture.color)
@@ -48,7 +48,7 @@ def capture_rgbd(dir_path, to_images):
                 }
 
                 # Save the dictionary as a .npy file
-                npy_path = os.path.join(dir_path, "rgbdk.npy")
+                npy_path = os.path.join(output_path, "rgbdk.npy")
                 np.save(npy_path, data_dict)
 
                 print(f"Saved RGBD data to {npy_path}")
@@ -67,4 +67,4 @@ if __name__ == "__main__":
     parser.add_argument('--to_images', action='store_true', help='Save RGB and Depth data as a single .npy file')
 
     args = parser.parse_args()
-    capture_rgbd(args.dir_path, args.to_images)
+    capture_rgbd(args.output_path, args.to_images)
