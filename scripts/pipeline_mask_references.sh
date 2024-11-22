4f61b4d262dff8476508917e3d93d6d49b94e39e
Jacinto <jacinto@lambdavector.wifi.local.cmu.edu>
2024-11-22 15:16:41 -0500

    minor changes and cleaning

--- scripts/pipeline_mask_references.sh
+++ scripts/pipeline_mask_references.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
+timestamp=$(date +"%Y%m%d_%H%M%S")
+output_dir=~/robot-grasp/data/mask_references/reference_$timestamp
+mkdir -p $output_dir
+
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate main_env
 cd utils
-# python capture_rgbdk.py --output_path ~/robot-grasp/data/mask_references/new_reference --name initial_scene
-# echo Sleeping for 4 seconds
-# sleep 4
-# echo Done sleeping
-# python capture_rgbdk.py --output_path ~/robot-grasp/data/mask_references/new_reference --name hand_grasping
+python capture_rgbdk.py --output_path $output_dir --name initial_scene
+echo Sleeping for 4 seconds
+sleep 4
+echo Done sleeping
+python capture_rgbdk.py --output_path $output_dir/hand_frames --name hand_grasping
 
 
-# conda deactivate
-# conda activate wilor
-# cd ~/robot-grasp/third_party/WiLoR
-# python demo_rgbdk.py --npy_folder ~/robot-grasp/data/mask_references/new_reference --out_folder ~/robot-grasp/data/mask_references/new_reference --save_mesh
+mkdir -p $output_dir/hand_frames
+conda deactivate
+conda activate wilor
+cd ~/robot-grasp/third_party/WiLoR
+python demo_rgbdk.py --npy_folder $output_dir/hand_frames --out_folder $output_dir/hand_frames --save_mesh
 
 conda deactivate
 conda activate main_env
 cd ~/robot-grasp/scripts/utils
-python visualize_rgbdk_or_obj.py ~/robot-grasp/data/mask_references/new_reference/initial_scene.npy ~/robot-grasp/data/mask_references/new_reference/hand_grasping_0.obj
-python generate_affordance_mask.py --initial_scene ~/robot-grasp/data/mask_references/new_reference/initial_scene.npy --hand ~/robot-grasp/data/mask_references/new_reference/hand_grasping_0.obj --output_dir ~/robot-grasp/data/mask_references/new_reference --output_dir ~/robot-grasp/data/mask_references/new_reference
+python visualize_rgbdk_or_obj.py $output_dir/initial_scene.npy $output_dir/hand_frames/hand_grasping_0.obj
+python generate_affordance_mask.py --initial_scene $output_dir/initial_scene.npy --hand $output_dir/hand_frames/hand_grasping_0.obj --output_dir $output_dir
